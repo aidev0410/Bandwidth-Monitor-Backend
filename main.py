1d2edcbedb8755094d2dc5d071507965da5b3bfd
aidev0410 <aidev0410@gmail.com>
2025-01-20 05:55:08 -0800

    Add logging configuration

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
-from fastapi import FastAPI
+import uvicorn
 
-app = FastAPI()
+from app.config.settings import settings
+
+if __name__ == "__main__":
+    uvicorn.run(
+        "app.app:app",
+        log_config=settings.logging_config,
+        reload=True,
+        host=settings.host,
+        port=settings.port,
+    )
